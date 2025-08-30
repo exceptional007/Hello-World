46a13dc7fe6e9044c3275a68aaf886fd1a76c1ec
exceptional007 <akshhatsri7843@gmail.com>
2025-08-30 12:40:24 +0530

    ADD Buddha Heading

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const buttoncolor = document.getElementById("btn");
 const head = document.getElementById("heading")
+const head_2 = document.getElementById("heading_2")
 buttoncolor.addEventListener('click', function () {
     head.style.color = 'red';
+    head_2.style.color = 'blue';
+    document.body.style.backgroundColor = 'black';
 })
